72dada9b563449da0df87005750473c5bb63b570
Alfredo Alcántara <alfredo.alcantara.r@gmail.com>
2019-11-29 07:28:34 -0800

    Add refresh button to FilterActivity

--- app/src/main/java/aar92_22/library/Activities/FilterActivity.java
+++ app/src/main/java/aar92_22/library/Activities/FilterActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -131,13 +133,23 @@ public class FilterActivity extends AppCompatActivity implements AdapterView.OnI
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == android.R.id.home) {
-            returnResult();
-            NavUtils.navigateUpFromSameTask(this);
 
+        switch (id){
+            case android.R.id.home:
+                returnResult();
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+
+            case R.id.action_refresh:
+                mAuthor.setSelection(0);
+                return true;
+
+            default: return super.onOptionsItemSelected(item);
 
         }
-        return super.onOptionsItemSelected(item);
+
+
+
     }
 
 
@@ -176,6 +188,16 @@ public class FilterActivity extends AppCompatActivity implements AdapterView.OnI
         } else {
             setResult(RESULT_CANCELED);
         }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+
+        menuInflater.inflate(R.menu.filter_activity_menu, menu);
+
+        return true;
     }
 
 
