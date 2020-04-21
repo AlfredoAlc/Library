e72cc1a8415c6627b8c8d9ea751249fe49a4a6ce
Alfredo Alcántara <alfredo.alcantara.r@gmail.com>
2020-04-21 11:09:43 -0700

    Add espresso test dependencies

--- app/src/androidTest/java/aar92_22/library/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/aar92_22/library/ExampleInstrumentedTest.java
@@ -1,22 +1,18 @@
 package aar92_22.library;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
 @RunWith(AndroidJUnit4.class)
-public class
-        ExampleInstrumentedTest {
+public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
