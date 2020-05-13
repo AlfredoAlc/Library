8c5b34105275ea31e3f3c27e7e1d4522f481a598
Alfredo Alcántara <alfredo.alcantara.r@gmail.com>
2020-05-13 10:36:25 -0700

    Adds tests

--- app/src/androidTest/java/aar92_22/library/AddBookActivityTest.java
+++ app/src/androidTest/java/aar92_22/library/AddBookActivityTest.java
@@ -1,4 +1,82 @@
 package aar92_22.library;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import aar92_22.library.Activities.AddBookActivity;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.isInternal;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.not;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class AddBookActivityTest {
+
+    private static final String TITLE = "Harry Potter";
+    private static final String LAST_NAME = "Rowling";
+    private static final String FIRST_NAME = "Joanne";
+
+    @Rule
+    public IntentsTestRule<AddBookActivity> mActivityRule = new IntentsTestRule<>(AddBookActivity.class);
+
+    @Before
+    public void stubAllExternalIntents() {
+        intending(not(isInternal())).respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, null));
+    }
+
+    @Test
+    public void clickAddNewAuthorAndRemoveAuthorTest (){
+        //Adds authors layout
+        onView(withId(R.id.add_author)).perform(click());
+        onView(withId(R.id.author_layout2)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
+        onView(withId(R.id.add_author)).perform(click());
+        onView(withId(R.id.author_layout3)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
+        //Removes authors layout
+        onView(withId(R.id.remove_author3)).perform(click());
+        onView(withId(R.id.author_layout3)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+
+        onView(withId(R.id.remove_author2)).perform(click());
+        onView(withId(R.id.author_layout2)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+
+    }
+
+    @Test
+    public void clickSaveBookTest(){
+        onView(withId(R.id.title_input)).perform(typeText(TITLE), closeSoftKeyboard());
+        onView(withId(R.id.last_name_input)).perform(replaceText(LAST_NAME));
+        onView(withId(R.id.first_name_input)).perform(replaceText(FIRST_NAME));
+        onView(withId(R.id.action_done)).perform(click());
+    }
+
+
+
+    @Test
+    public void clickTakePhotoTest(){
+        onView(withId(R.id.add_book_scroll_view)).perform(swipeUp());
+        onView(withId(R.id.take_photo)).perform(click());
+        onView(withId(R.id.select_image)).check(matches(isDisplayed()));
+        onView(withId(R.id.take_photo)).check(matches(isDisplayed()));
+    }
+
 }
--- app/src/androidTest/java/aar92_22/library/MainActivityTest.java
+++ app/src/androidTest/java/aar92_22/library/MainActivityTest.java
@@ -5,45 +5,43 @@ import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Intent;
 
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.intent.ActivityResultFunction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
-
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import aar92_22.library.Activities.AddBookActivity;
+import aar92_22.library.Activities.BookDetailActivity;
 import aar92_22.library.Activities.MainActivity;
 
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.Intents.intending;
-import static androidx.test.espresso.intent.matcher.ComponentNameMatchers.hasClassName;
-import static androidx.test.espresso.intent.matcher.ComponentNameMatchers.hasPackageName;
 import static androidx.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasPackage;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.isInternal;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
-import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withTagKey;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.AllOf.allOf;
 
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class MainActivityTest {
 
+    private static final Integer BOOK_POSITION = 1;
+
     @Rule
-    public IntentsTestRule<MainActivity> mActivityRule = new IntentsTestRule<>(MainActivity.class);
+    public IntentsTestRule<MainActivity> intentsTestRule = new IntentsTestRule<>(MainActivity.class);
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
 
     @Before
     public void stubAllExternalIntents() {
@@ -54,9 +52,7 @@ public class MainActivityTest {
     public void clickAddButtonTest (){
         //Opens Add Book Activity
         onView(withId(R.id.add_book_button)).perform(click());
-        intending(hasComponent(hasShortClassName(".AddBookActivity"))).respondWith(
-                new Instrumentation.ActivityResult(Activity.RESULT_OK, null)
-        );
+        intended(hasComponent(hasShortClassName(".AddBookActivity")));
 
     }
 
@@ -69,11 +65,27 @@ public class MainActivityTest {
 
     }
 
-    @Test
+   /* @Test
     public void clickBestSellersTest (){
         onView(withId(R.id.best_sellers_button)).perform(click());
         intending(hasComponent(hasShortClassName(".BestSellers"))).respondWith(
                 new Instrumentation.ActivityResult(Activity.RESULT_OK, null)
         );
-    }
+    }*/
+
+    /*@Test
+    public void openBookDetailFromRecyclerViewTest(){
+        //Must run AddBookActivityTest FIRST to run this test
+        onView(withId(R.id.list_books_recycler_view)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(BOOK_POSITION, click())
+        );
+
+        intending(allOf(hasComponent(hasShortClassName(".BookDetailActivity")),
+                hasExtraWithKey(BookDetailActivity.EXTRA_ID))).respondWith(
+                        new Instrumentation.ActivityResult(Activity.RESULT_OK, null)
+        );
+
+    }*/
+
+
 }
